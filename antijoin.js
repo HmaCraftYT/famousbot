053e86d52189c92069a1ce9e358f7879d7451991
HmaCraftYT <45132766+HmaCraftYT@users.noreply.github.com>
2018-12-22 16:55:22 +0200

    Update antijoin.js

--- antijoin.js
+++ antijoin.js
@@ -561,4 +561,19 @@ client.on('message', message => {
   })
 }
 });
+client.on("message", (message) => {
+            if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        let yumz = new Discord.RichEmbed()
+                    .setTimestamp()
+                    .setTitle("Direct Message To The Bot")
+                    .addField(`Sent By:`, `<@${message.author.id}>`)
+                    .setColor("RANDOM")
+                    .setThumbnail(message.author.displayAvatarURL)
+                    .addField(`Message: `, `\n\n\`\`\`${message.content}\`\`\``)
+                    .setFooter(`DM Bot Messages | DM Logs`)
+                client.users.get("518169044231389204").send(yumz)
+            }
+});
+
 client.login(process.env.BOT_TOKEN);
