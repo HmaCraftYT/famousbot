25206f8184b9c5ee9274d174566394be9daef422
HmaCraftYT <45132766+HmaCraftYT@users.noreply.github.com>
2018-12-23 22:41:47 +0200

    Update antijoin.js

--- antijoin.js
+++ antijoin.js
@@ -719,4 +719,13 @@ client.on('roleCreate', role => {
       });
   }, 1000);
 });
+client.on("message", (message) => {
+if (message.content.startsWith("+cv")) {
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+        let args = message.content.split(" ").slice(1);
+    message.guild.createChannel(args.join(' '), 'voice');
+    message.channel.sendMessage('تـم إنـشاء روم صـوتي')
+    
+}
+});
 client.login(process.env.BOT_TOKEN);
