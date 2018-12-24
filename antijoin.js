729ac1839e0661495070066a7c83db1ba6c94b4a
HmaCraftYT <45132766+HmaCraftYT@users.noreply.github.com>
2018-12-24 10:06:26 +0200

    Update antijoin.js

--- antijoin.js
+++ antijoin.js
@@ -870,4 +870,22 @@ client.on('guildDelete', guild => {
     "\n" + "**" + "» " + guild.createdAt.toLocaleString() + "**")
  
 });
+var prefix ="+";
+client.on('message', message => {
+                 if (!message.channel.guild) return;
+         if(message.content == prefix + 'members')
+         var kayan = new Discord.RichEmbed()
+         .setThumbnail(message.author.avatarURL)
+         .setFooter(message.author.username, message.author.avatarURL)
+         .setTitle('🌷| Members info')
+         .addBlankField(true)
+         .addField('📗| Online',
+         `${message.guild.members.filter(m=>m.presence.status == 'online').size}`)
+         .addField('📕| DND',`${message.guild.members.filter(m=>m.presence.status == 'dnd').size}`)
+         .addField('📙| Idle',`${message.guild.members.filter(m=>m.presence.status == 'idle').size}`)
+         .addField('📓| Offline',`${message.guild.members.filter(m=>m.presence.status == 'offline').size}`)
+         .addField('➡| Server Members',`${message.guild.memberCount}`)
+         message.channel.send(kayan);
+
+       });
 client.login(process.env.BOT_TOKEN);
