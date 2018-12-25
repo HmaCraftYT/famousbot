ba8b65d0ec983ed6d7c23e4ea2ef91bca3775992
HmaCraftYT <45132766+HmaCraftYT@users.noreply.github.com>
2018-12-25 21:23:18 +0200

    Update antijoin.js

--- antijoin.js
+++ antijoin.js
@@ -1088,10 +1088,12 @@ client.on('message',async msg => {
            var ccc =msg.guild.createChannel('Server Stats', 'voice').then(al => {
                 var aa =msg.guild.createChannel('Server Stats', 'voice').then(alp => {
                    var aaa =msg.guild.createChannel('Server Stats', 'voice').then(alph => {
+			   var aaaa =msg.guild.createChannel('Server Stats', 'voice').then(alpha => {
 			   
        al.setParent(kk);
        alp.setParent(kk);
        alph.setParent(kk);
+       alpha.setParents(kk);			   
      al.overwritePermissions(msg.guild.id, {
       CONNECT: false,
       SPEAK: false
@@ -1104,6 +1106,10 @@ client.on('message',async msg => {
       CONNECT: false,
       SPEAK: false
     });
+     alpha.overwritePermissions(msg.guild.id, {
+     CONNECT: false,
+     SPEAK: false
+    });			   
   setInterval(() => {
       var currentTime = new Date(),
 hours = currentTime.getHours() + 2 ,
@@ -1126,7 +1132,7 @@ hours = 12;
      al.setName(`Voice Online :[ ${msg.guild.members.filter(m => m.voiceChannel).size} ]`);
       alp.setName(`Time :[${hours} : ${minutes} : ${Seconds} ${suffix}]`);
         alph.setName(`[ Date : [${Year} - ${Month} - ${Dat} ]`);
-	  
+	alpha.setName(`Members [${message.guild.memberCount}]`);  
  },1000);
                    })
     
