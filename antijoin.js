e025260452a5da0c43145ec3fc45c847476d03a2
HmaCraftYT <45132766+HmaCraftYT@users.noreply.github.com>
2018-12-28 16:24:13 +0200

    Update antijoin.js

--- antijoin.js
+++ antijoin.js
@@ -4,13 +4,6 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!,script by HemaCraft`);
 });
- 
- client.on("ready", () => { // ßæÏ ÑíäÈæ
-  function lol() {
-    client.guilds.get('525263074190426112').roles.find("name", "rainbow").setColor("RANDOM");
-  };
-  setInterval(lol, 5000);
-});
 var prefix = '+';
 
 const developers = ["518169044231389204","id"]
@@ -1166,4 +1159,58 @@ client.on('message', message => {
         message.author.sendEmbed(embed);
     }
 });
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
+const suck = JSON.parse(fs.readFileSync('./suck.json', 'utf8'));
+const prefix = "r+";
+// By M7MD
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag} !`);
+          client.user.setActivity("Rainbow , r#help .",{type: 'WATCHING'});
+  
+  });
+client.on("message", message => {
+    fs.writeFile('./suck.json', JSON.stringify(suck));
+});
+
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.forEach(g => {
+            if (suck[g.id]) {
+                if (suck[g.id].role) {
+                    var role = g.roles.get(suck[g.id].role);
+                    if (role) {
+                        role.edit({color : "RANDOM"});
+                    };
+                };
+            };
+        });
+    }, 4000);
+})
+
+client.on("message", message => {
+    if (!message.content.startsWith(prefix)) return;
+    if (message.author.bot) return;
+    if (message.channel.type !== "text") return message.reply("This Command Is Only Allowed In Servers");
+    var args = message.content.split(" ");
+    var command = args[0].slice(prefix.length);
+    switch(command) {
+        case "set" :
+        if(!message.member.hasPermission('ADMINSTRATOR')) return message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINSTRATOR`' );
+        message.guild.createRole({name : "RainbowBot .", color : "RANDOM"}).then(r => {
+            r.edit({color : "RANDOM"});
+            suck[message.guild.id] = {role : r.id};
+        });
+    };
+});
+client.on("message", message => {
+  if (message.content === "r+help") {
+      message.react('🌈')
+message.author.send(`**
+r+set 
+ - لإنشاء رتبة الرينبو وبدا الرينبو
+- To create the role of the Rainbow & Start The Rainbow
+**`)
+  }})
 client.login(process.env.BOT_TOKEN);
