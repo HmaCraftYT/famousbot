ddc062e11b8716f637ae247acfdee99e0839e66b
HmaCraftYT <45132766+HmaCraftYT@users.noreply.github.com>
2018-12-29 11:44:49 +0200

    Update antijoin.js

--- antijoin.js
+++ antijoin.js
@@ -1208,4 +1208,32 @@ r+set
 - To create the role of the Rainbow & Start The Rainbow
 **`)
   }})
+client.on('message', message => {//Toxic Codes
+    if (message.author.bot) return;
+     if (message.content === prefix + "email") {//Toxic Codes
+function randomem() {//Toxic Codes
+let email = '';//Toxic Codes
+const n3k4a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._"\'';
+for (let i = 0; i < 5; i++) email += n3k4a.charAt(Math.floor(Math.random() * n3k4a.length));//Toxic Codes
+return email//Toxic Codes
+}//Toxic Codes//Toxic Codes
+function randompass() {//Toxic Codes
+let pass = '';//Toxic Codes
+const Toxic Codes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?@#$%&()-_"\'';//Toxic Codes
+for (let i = 0; i < 8; i++) pass += Toxic Codes.charAt(Math.floor(Math.random() * Toxic Codes.length));//Toxic Codes
+return pass;
+}
+const random1 = randomem();//Toxic Codes//Toxic Codes
+const random2 = randompass();//Toxic Codes
+message.author.send(`------------------------//Toxic Codes
+email : **${random1}@gmail.com**//Toxic Codes
+password : **${random2}**//Toxic Codes
+------------------------`).catch(err => {//Toxic Codes
+   if(err == "DiscordAPIError: Cannot send messages to this user") {//Toxic Codes
+      return message.channel.send("**للأسف , لديك اعدادات خصوصية لاتسمح لي بأرسال رسائل خاصة لك **");//Toxic Codes
+}//Toxic Codes
+});//Toxic Codes
+//Toxic Codes
+message.channel.send("**تم الارسال الحساب في الخاص | ☑ **")//Toxic Codes
+});//Toxic Codes
 client.login(process.env.BOT_TOKEN);
