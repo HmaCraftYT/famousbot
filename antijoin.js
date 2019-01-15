8b04889e388250effdd6c1ac06cdfe5d5f7feb43
HmaCraftYT <45132766+HmaCraftYT@users.noreply.github.com>
2019-01-15 16:26:33 +0200

    Update antijoin.js

--- antijoin.js
+++ antijoin.js
@@ -1181,4 +1181,13 @@ client.on('message',async msg => {//Toxic Codes
   }
  
 });
+var prefix ="+";
+client.on("message", (message) => {
+    if(message.content.startsWith(prefix+"gmail")) {
+        message.channel.send(JSON.stringify({
+            email: Math.random().toString(36).slice(4).trim()+"@gmail.com",
+            password: Math.random().toString(36).slice(4).trim()
+        }))
+    }
+})
 client.login(process.env.BOT_TOKEN);
