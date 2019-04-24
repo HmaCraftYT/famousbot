da5913b8a1c864cc5536f1729ffdd2672795e0a4
HmaCraftYT <45132766+HmaCraftYT@users.noreply.github.com>
2019-04-25 01:26:33 +0200

    Update antijoin.js

--- antijoin.js
+++ antijoin.js
@@ -1486,7 +1486,7 @@ client.on('message', message => {
       .setThumbnail(message.author.iconURL)
       .setFooter(message.author.username, message.author.avatarURL)
       .setTitle(' عدد اعضاء السيرفر  ')
-      .addblankfield(true)
+      .addblankfield(`false`)
       .addField(`${message.guild.memberCount}`)
       message.channel.send(IzRo);
     });
